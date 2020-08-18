3ad7a31bb489100321d0a682df692f4cf7a66c25
zodiac12k <zodiac12k@gmail.com>
2020-08-18 14:13:08 +0900

    Upgrade keycloak to support k8s v1.16 or higher

--- zcp-keycloak/install_eks.sh
+++ zcp-keycloak/install_eks.sh
@@ -2,10 +2,23 @@
 
 kubectl create secret generic realm-secret -n ${TARGET_NAMESPACE} --from-file=realm-zcp-export.json
 
-# Install keycloak and postgresql for keycloak
-# Chart revision for keycloak : https://github.com/helm/charts/tree/8f5c3426d9597a902d57bea378dc628ea2b91c20/stable/keycloak
-# Chart revision for postgresql : https://github.com/helm/charts/tree/93ccc2350550ff5d95f13a5c25dc54babc44c048/stable/postgresql
-helm install stable/keycloak --version 2.0.0 \
+# Install postgresql for keycloak
+# Chart revision for postgresql : https://github.com/bitnami/charts/tree/47b3f22873a2e622442ae7962d19a07b74b5de91/bitnami/postgresql
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install bitnami/postgresql --version 9.1.4 \
+--name zcp-oidc-postgresql \
+-f values-postgresql.yaml \
+--namespace ${TARGET_NAMESPACE} \
+--set postgresqlUsername=${KEYCLOAK_DB_USER} \
+--set postgresqlPassword=${KEYCLOAK_DB_PWD} \
+--set postgresqlDatabase=${KEYCLOAK_DB_NAME} \
+--set persistence.enabled=true \
+--set persistence.storageClass=ebs-gp2-retain \
+--set persistence.size=5Gi
+
+# Install keycloak
+# Chart revision for keycloak : https://github.com/helm/charts/tree/577a48db1bb1fd6fa931b74f411b83526a598cb1/stable/keycloak
+helm install stable/keycloak --version 4.0.2 \
 --name zcp-oidc \
 -f values-eks.yaml \
 --namespace ${TARGET_NAMESPACE} \
@@ -15,10 +28,11 @@ helm install stable/keycloak --version 2.0.0 \
 --set keycloak.ingress.annotations."kubernetes\.io/ingress\.class"=${KEYCLOAK_INGRESS_CONTROLLER} \
 --set keycloak.ingress.tls[0].hosts[0]=${KEYCLOAK_INGRESS_HOSTS} \
 --set keycloak.ingress.tls[0].secretName=${DOMAIN_SECRET_NAME} \
---set postgresql.postgresPassword=${KEYCLOAK_DB_PWD} \
---set postgresql.persistence.enabled=true \
---set postgresql.persistence.storageClass=efs-zcp \
---set postgresql.persistence.size=5Gi
+--set keycloak.persistence.dbPassword=${KEYCLOAK_DB_PWD} \
+#--set postgresql.postgresPassword=${KEYCLOAK_DB_PWD} \
+#--set postgresql.persistence.enabled=true \
+#--set postgresql.persistence.storageClass=efs-zcp \
+#--set postgresql.persistence.size=5Gi
 #--set keycloak.resources.limits.cpu=${KEYCLOAK_LIMIT_CPU} \
 #--set keycloak.resources.limits.memory=${KEYCLOAK_LIMIT_MEM} \
 #--set keycloak.resources.requests.cpu=${KEYCLOAK_REQUEST_CPU} \
