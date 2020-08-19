801516abbd02fe5933c1a75727ff1508fc99f017
Minyoung Song <37865497+MinyoungSong@users.noreply.github.com>
2020-08-19 15:53:53 +0900

    k8s v.1.17 설치 테스트 - Git, Registry (#10)
    * k8s 1.17 git, registry 테스트 결과 적용
    
    iks, eks - 테스트 완료
    aks - 테스트 대기중(aks cluster 생성 전)
    
    * DOMAIN_SECRET_NAME 추가, trivy 설치 false로 변경
    
    * registry readme 정리 및 임시 파일 정리
    
    * zcp-git readme 및 임시파일 정리
    
    * cloudzcp-io-cert 로 변경
    
    * helm3 설치 시 인터넷 web에서 다운로드 받도록 수정, helm3 최소 버전 수정
    
    * gitea chart를 zcp repo에 올린 후 install.sh 변경
    
    * harbor helm 1.4.2 chart 백업 삭제
    
    * web download 로 변경함에 따라 불필요.
    
    * gitea 최신 버전 chart 삭제(package 된거와 중복)

--- zcp-git/install_aks.sh
+++ zcp-git/install_aks.sh
@@ -3,11 +3,11 @@
 # Install gitea
 # Chart revision for jenkins : https://github.com/helm/charts/tree/605d3d441e6515fcbbf8a69bdc69794d90bbfd5c/stable/jenkins
 helm3 install zcp-git zcp/zcp-gitea \
--f values-iks.yaml \
+-f values-aks.yaml \
 --namespace ${TARGET_NAMESPACE} \
---set ingress.annotations."ingress\.bluemix\.net/ALB-ID"=${GITEA_INGRESS_CONTROLLER} \
+--set ingress.annotations."kubernetes\.io/ingress\.class"=${GITEA_INGRESS_CONTROLLER} \
 --set ingress.hostname=${GITEA_INGRESS_HOSTS} \
 --set ingress.tls[0].hosts[0]=${GITEA_INGRESS_TLS_HOSTS} \
 --set ingress.tls[0].secretName=${DOMAIN_SECRET_NAME} \
---set persistence.storageClass=ibmc-file-silver \
---set mariadb.master.persistence.storageClass=ibmc-block-silver \
+--set persistence.storageClass=file-standard \
+--set mariadb.master.persistence.storageClass=managed-standard \
