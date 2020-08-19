e47d3e55e24ded9ba999b061bcb9ffc6eeb1584a
earth1223 <genearth7@gmail.com>
2020-08-19 16:06:06 +0900

    Update Jenkins scripts to support AKS (#12)
    * Support aks
    
    * Modify shell script values key

--- zcp-jenkins/install_aks.sh
+++ zcp-jenkins/install_aks.sh
@@ -4,7 +4,7 @@
 # Chart revision for jenkins : https://github.com/helm/charts/tree/605d3d441e6515fcbbf8a69bdc69794d90bbfd5c/stable/jenkins
 helm install jenkins-0.14.3-117.tgz --version 0.14.3 \
 --name zcp-jenkins \
--f values-eks.yaml \
+-f values-aks.yaml \
 --namespace ${TARGET_NAMESPACE} \
 --set Master.AdminUser=${JENKINS_ADMIN_ID} \
 --set Master.AdminPassword=${JENKINS_ADMIN_PWD} \
@@ -12,6 +12,6 @@ helm install jenkins-0.14.3-117.tgz --version 0.14.3 \
 --set Master.Ingress.Annotations."kubernetes\.io/ingress\.class"=${JENKINS_INGRESS_CONTROLLER} \
 --set Master.Ingress.TLS[0].hosts[0]=${JENKINS_INGRESS_TLS_HOSTS} \
 --set Master.Ingress.TLS[0].secretName=${DOMAIN_SECRET_NAME} \
---set Persistence.StorageClass=efs-zcp
+--set Persistence.StorageClass=managed-standard-retain
 
-kubectl create -f zcp-jenkins-maven-eks-pvc.yaml
+kubectl create -f zcp-jenkins-maven-aks-pvc.yaml
