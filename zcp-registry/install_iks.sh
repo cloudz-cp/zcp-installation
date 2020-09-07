ede554ef7458bc591abe7d7132844ef7de5b9e3d
MinyoungSong <lievel@naver.com>
2020-09-07 16:54:56 +0900

    job service sc를 block으로 변경, 다른 sc 들을 retain으로 변경

--- zcp-registry/install_iks.sh
+++ zcp-registry/install_iks.sh
@@ -23,11 +23,11 @@ helm3 install zcp-registry harbor/harbor --version 1.4.2 \
 --set externalURL=${HARBOR_EXTERNAL_URL} \
 --set expose.tls.secretName=${DOMAIN_SECRET_NAME} \
 --set harborAdminPassword=${HARBOR_ADMIN_PASSWORD} \
---set persistence.persistentVolumeClaim.jobservice.storageClass=ibmc-file-silver \
+--set persistence.persistentVolumeClaim.jobservice.storageClass=ibmc-block-retain-silver \
 --set persistence.persistentVolumeClaim.jobservice.size=20Gi \
---set persistence.persistentVolumeClaim.database.storageClass=ibmc-block-silver \
+--set persistence.persistentVolumeClaim.database.storageClass=ibmc-block-retain-silver \
 --set persistence.persistentVolumeClaim.database.size=20Gi \
---set persistence.persistentVolumeClaim.redis.storageClass=ibmc-block-silver \
+--set persistence.persistentVolumeClaim.redis.storageClass=ibmc-block-retain-silver \
 --set persistence.persistentVolumeClaim.redis.size=20Gi \
 --set persistence.imageChartStorage.s3.accesskey=${HARBOR_S3_ACCESSKEY} \
 --set persistence.imageChartStorage.s3.secretkey=${HARBOR_S3_SECRETKEY} \
