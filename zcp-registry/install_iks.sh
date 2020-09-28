0963f4e3ab4b3111119736cfef8a60eb2beae38f
MinyoungSong <lievel@naver.com>
2020-09-28 13:57:27 +0900

    registry.au-syd.bluemix.net 바라보도록 harbor 수정

--- zcp-registry/install_iks.sh
+++ zcp-registry/install_iks.sh
@@ -12,6 +12,7 @@ HARBOR_S3_ACCESSKEY=
 HARBOR_S3_SECRETKEY=
 HARBOR_S3_BUCKET=
 HARBOR_S3_REGIONENDPOINT=
+IMAGE_PULL_SECRET=
 
 # Install harbor
 helm3 repo add harbor https://helm.goharbor.io
@@ -35,3 +36,18 @@ helm3 install zcp-registry harbor/harbor --version 1.4.2 \
 --set persistence.imageChartStorage.s3.secretkey=${HARBOR_S3_SECRETKEY} \
 --set persistence.imageChartStorage.s3.bucket=${HARBOR_S3_BUCKET} \
 --set persistence.imageChartStorage.s3.regionendpoint=${HARBOR_S3_REGIONENDPOINT} \
+--set imagePullSecrets[0].name=${IMAGE_PULL_SECRET} \
+--set nginx.image.repository=registry.au-syd.bluemix.net/cloudzcp/nginx-photon \
+--set portal.image.repository=registry.au-syd.bluemix.net/cloudzcp/harbor-portal \
+--set core.image.repository=registry.au-syd.bluemix.net/cloudzcp/harbor-core \
+--set jobservice.image.repository=registry.au-syd.bluemix.net/cloudzcp/harbor-jobservice \
+--set registry.registry.image.repository=registry.au-syd.bluemix.net/cloudzcp/registry-photon \
+--set registry.controller.image.repository=registry.au-syd.bluemix.net/cloudzcp/harbor-registryctl \
+--set chartmuseum.image.repository=registry.au-syd.bluemix.net/cloudzcp/chartmuseum-photon \
+--set clair.clair.image.repository=registry.au-syd.bluemix.net/cloudzcp/clair-photon \
+--set clair.adapter.image.repository=registry.au-syd.bluemix.net/cloudzcp/clair-adapter-photon \
+--set trivy.image.repository=registry.au-syd.bluemix.net/cloudzcp/trivy-adapter-photon \
+--set notary.server.image.repository=registry.au-syd.bluemix.net/cloudzcp/notary-server-photon \
+--set notary.signer.image.repository=registry.au-syd.bluemix.net/cloudzcp/notary-signer-photon \
+--set database.internal.image.repository=registry.au-syd.bluemix.net/cloudzcp/harbor-db \
+--set redis.internal.image.repository=registry.au-syd.bluemix.net/cloudzcp/redis-photon \
